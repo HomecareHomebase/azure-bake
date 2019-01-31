500b5a7373d3145e59a8672589ad07661fa4deda
whilke <whilke@gmail.com>
2019-01-31 10:48:17 -0600

    fix(system): Fixed #12 - bake.yaml with no variables defined was failing

--- system/src/bake-loader.ts
+++ system/src/bake-loader.ts
@@ -77,6 +77,13 @@ export class BakePackage {
 
     private objToVariableMap(obj: any) {
         let strMap = new Map<string,BakeVariable>();
+
+        //support variables being empty, or not defined in the YAML.
+        if (obj == null || undefined)
+        {
+            return strMap
+        }
+
         for (let k of Object.keys(obj)) {
             strMap.set(k, new BakeVariable(obj[k]));
         }
