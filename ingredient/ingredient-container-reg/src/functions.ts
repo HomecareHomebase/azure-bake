9cb8a1a7a8a326adc96da2d5cf31342775e83331
Charles Sperbeck <csperbeck@hchb.com>
2019-03-26 09:51:53 -0400

    added reflection to ACR

---
+++ ingredient/ingredient-container-reg/src/functions.ts
@@ -0,0 +1,23 @@
+import {BaseUtility, IngredientManager} from '@azbake/core'
+
+export class TrafficUtils extends BaseUtility {
+
+    public get_profile(): string {
+
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        const acr_profile = util.create_resource_name("acr", null, false);
+        return acr_profile;
+    }
+    
+    public get_region(): string {
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        const acr_region = util.current_region()
+        return acr_region
+    }
+
+    public get_primary_region(): string {
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        const acr_primary_region = util.current_region_primary()
+        return acr_primary_region
+    }
+}
