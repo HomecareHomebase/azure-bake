c32e23c519564f242ab144febdcb9e4a9018b99d
Charles Sperbeck <csperbeck@hchb.com>
2019-03-26 09:58:42 -0400

    added reflection to storage

--- ingredient/ingredient-storage/src/functions.ts
+++ ingredient/ingredient-storage/src/functions.ts
@@ -1,5 +1,25 @@
 import {BaseUtility, IngredientManager} from '@azbake/core'
 
 export class StorageUtils extends BaseUtility {
+
+    public get_profile(): string {
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        const st_profile = util.create_resource_name("st", null, false);
+        return st_profile;
+    }
+    
+    public get_region(): string {
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        const st_region = util.current_region()
+        return st_region
+    }
+
+    public get_primary_region(): string {
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        const st_primary_region = util.current_region_primary()
+        return st_primary_region
+    }
+}
+
     
 }
