0d96db8252db2e02a90f4c7044ff34aaa9d316f0
Charles Sperbeck <csperbeck@hchb.com>
2019-04-02 10:44:45 -0400

    Refactored storage functions

--- ingredient/ingredient-storage/src/functions.ts
+++ ingredient/ingredient-storage/src/functions.ts
@@ -2,21 +2,10 @@ import {BaseUtility, IngredientManager} from '@azbake/core'
 
 export class StorageUtils extends BaseUtility {
 
-    public get_profile(): string {
+    public create_resource_name(): string {
         let util = IngredientManager.getIngredientFunction("coreutils", this.context)
         const st_profile = util.create_resource_name("st", null, false);
         return st_profile;
     }
-    
-    public get_region(): string {
-        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
-        const st_region = util.current_region()
-        return st_region
-    }
-
-    public get_primary_region(): string {
-        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
-        const st_primary_region = util.current_region_primary()
-        return st_primary_region
-    }
 }
+    
