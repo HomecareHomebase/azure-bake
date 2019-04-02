45ad2fc33840655d1989370699f4c56f61fddc6b
Charles Sperbeck <csperbeck@hchb.com>
2019-04-02 10:44:32 -0400

    Refactored ACR functions

--- ingredient/ingredient-container-reg/src/functions.ts
+++ ingredient/ingredient-container-reg/src/functions.ts
@@ -2,22 +2,10 @@ import {BaseUtility, IngredientManager} from '@azbake/core'
 
 export class ContainerRegUtils extends BaseUtility {
 
-    public get_profile(): string {
+    public create_resource_name(): string {
 
         let util = IngredientManager.getIngredientFunction("coreutils", this.context)
         const acr_profile = util.create_resource_name("acr", null, false);
         return acr_profile;
-    }
-    
-    public get_region(): string {
-        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
-        const acr_region = util.current_region()
-        return acr_region
-    }
-
-    public get_primary_region(): string {
-        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
-        const acr_primary_region = util.current_region_primary()
-        return acr_primary_region
-    }
+    }    
 }
