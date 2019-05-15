c69593a9f430d4131a04dfdabcb4a08ded7e0a01
Mo Cassidy <mcassidy@hchb.com>
2019-05-15 09:41:54 -0500

    replace in file

--- ingredient/ingredient-kubernetes/src/plugin.ts
+++ ingredient/ingredient-kubernetes/src/plugin.ts
@@ -1,13 +1,15 @@
 import { BaseIngredient, IngredientManager } from "@azbake/core"
 import { promisify } from 'util';
 import { exec as exec_from_child_process } from 'child_process';
+import Replace from 'replace-in-file';
 const exec = promisify(exec_from_child_process);
 
 export class KubernetesPlugin extends BaseIngredient {
-
+    
     public async Execute(): Promise<void> {
         try {
-            let k8sYamlPath = await this._ingredient.properties.source.valueAsync(this._ctx);
+            let k8sYamlPath = await this._ingredient.properties.source.valueAsync(this._ctx);            
+            await this.replaceTokens(k8sYamlPath);
             let testDeployment = this._ingredient.properties.parameters.get("testDeployment");
             let k8sConfigPath = this._ingredient.properties.parameters.get("config");
             let configParam = "";
@@ -25,6 +27,34 @@ export class KubernetesPlugin extends BaseIngredient {
         } catch(error){
             this._logger.error('deployment failed: ' + error)
             throw error
+        }
+    }
+    private async replaceTokens(path: any): Promise<void>{
+        const openingDelimiter = "{{"
+        const closingDelimiter = "}}"
+        console.log(path);
+        const envKeys : RegExp[] = []
+        const envVals : string[] = []
+        for (const key in process.env) {
+            if (process.env.hasOwnProperty(key)) {
+                const value = process.env[key];
+                envKeys.push(new RegExp(openingDelimiter+key+closingDelimiter,"g"))
+                envVals.push(value || "")
+            }
+        }
+        // TODO: map dir path to globs (i.e., if isDirectory(...))
+        const options = {
+            files: path,
+            from: envKeys,
+            to: envVals,
+        };
+        try {
+            const results = await Replace(options)
+            // TODO Remove
+            this._logger.log('Replacement results:', results);
+        }
+        catch (error) {
+            this._logger.error('Error occurred:', error);
         }
     }
 }
