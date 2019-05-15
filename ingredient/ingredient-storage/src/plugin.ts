85e2854c5fe6c32764b8e69e66a758c8e86807eb
Brian Schaap <bschaap@hchb.com>
2019-05-15 18:10:06 -0400

    minor refactor

--- ingredient/ingredient-storage/src/plugin.ts
+++ ingredient/ingredient-storage/src/plugin.ts
@@ -18,104 +18,108 @@ export class StoragePlugIn extends BaseIngredient {
             
             await helper.DeployTemplate(this._name, ARMTemplate, params, await util.resource_group())
 
-            
-            let accountName: string;
-            let accountKey: string;
+            await this.ConfigureDiagnosticSettings(params, util);
 
-            //Get blob storage properties
-            accountName = params["storageAccountName"].value;
-            const storageUtils = new StorageUtils(this._ctx);
-            accountKey = await storageUtils.get_primary_key(accountName, await util.resource_group())
-            const credentials = new SharedKeyCredential(accountName, accountKey);
-            const pipeline = StorageURL.newPipeline(credentials, {
-              // Enable logger when debugging
-              // logger: new ConsoleHttpPipelineLogger(HttpPipelineLogLevel.INFO)
-            });            
-            const blobPrimaryURL = `https://${accountName}.blob.core.windows.net/`;
-            var serviceURL = new ServiceURL(blobPrimaryURL, pipeline)
-            const serviceProperties = await serviceURL.getProperties(Aborter.none);
-
-            //Get Bake variables for diagnostic settings.  Default to "true" (enabled) and 10 days data retention.
-            let blobDiagnosticHourlyMetricsEnabled: string = await util.variable("blobDiagnosticHourlyMetricsEnabled") || "true"
-            let blobDiagnosticHourlyMetricsRetentionDays = await util.variable("blobDiagnosticHourlyMetricsRetentionDays") || 10    
-            let blobDiagnosticMinuteMetricsEnabled: string = await util.variable("blobDiagnosticMinuteMetricsEnabled") || "true"
-            let blobDiagnosticMinuteMetricsRetentionDays = await util.variable("blobDiagnosticMinuteMetricsRetentionDays") || 10    
-            let blobDiagnosticLoggingEnabled: string = await util.variable("blobDiagnosticLoggingEnabled") || "true"
-            let blobDiagnosticLoggingRetentionDays = await util.variable("blobDiagnosticLoggingRetentionDays") || 10    
-
-            //Workaround due to issues using boolean data type for Bake variables
-            var boolBlobDiagnosticHourlyMetricsEnabled: boolean = (blobDiagnosticHourlyMetricsEnabled == "true") ? true : false;
-            var boolBlobDiagnosticMinuteMetricsEnabled: boolean = (blobDiagnosticMinuteMetricsEnabled == "true") ? true : false;
-            var boolBlobDiagnosticLoggingEnabled: boolean = (blobDiagnosticLoggingEnabled == "true") ? true : false;
-
-            //Debug logging of Bake variables
-            this._logger.debug("blobDiagnosticHourlyMetricsEnabled:" + boolBlobDiagnosticHourlyMetricsEnabled)
-            this._logger.debug("blobDiagnosticHourlyMetricsRetentionDays:" + blobDiagnosticHourlyMetricsRetentionDays)
-            this._logger.debug("blobDiagnosticMinuteMetricsEnabled:" + boolBlobDiagnosticMinuteMetricsEnabled)
-            this._logger.debug("blobDiagnosticMinuteMetricsRetentionDays:" + blobDiagnosticMinuteMetricsRetentionDays)
-            this._logger.debug("blobDiagnosticLoggingEnabled:" + boolBlobDiagnosticLoggingEnabled)
-            this._logger.debug("blobDiagnosticLoggingRetentionDays:" + blobDiagnosticLoggingRetentionDays)
-
-            //Configure hourly metric settings
-            serviceProperties.hourMetrics = {
-                enabled: boolBlobDiagnosticHourlyMetricsEnabled,
-                retentionPolicy: {
-                    days: blobDiagnosticHourlyMetricsRetentionDays,
-                    enabled: true
-                },
-                version: "1.0"
-            };
-
-            //Azure will error if includeAPIs is set when the metrics diagnostics setting is not enabled
-            if (boolBlobDiagnosticHourlyMetricsEnabled) {
-                serviceProperties.hourMetrics.includeAPIs = true;
-            }
-
-            //Configure minute metric settings
-            serviceProperties.minuteMetrics = {
-                enabled: boolBlobDiagnosticMinuteMetricsEnabled,
-                retentionPolicy: {
-                    days: blobDiagnosticMinuteMetricsRetentionDays,
-                    enabled: true
-                },
-                version: "1.0"
-            };
-
-            //Azure will error if includeAPIs is set when the metrics diagnostics setting is not enabled
-            if (boolBlobDiagnosticMinuteMetricsEnabled) {
-                serviceProperties.minuteMetrics.includeAPIs = true;
-            }
-
-            //Configure logging settings
-            serviceProperties.logging = {
-                deleteProperty: boolBlobDiagnosticLoggingEnabled,
-                read: boolBlobDiagnosticLoggingEnabled,
-                retentionPolicy: {
-                    days: blobDiagnosticLoggingRetentionDays,
-                    enabled: true
-                },
-                version: "2.0",
-                write: boolBlobDiagnosticLoggingEnabled
-            };
-
-            //The Azure SDK will error with "storageServiceProperties.Cors must be of type Array." if no CORS value is specified.  Set to a restrictive setting.
-            //See: https://github.com/Azure/azure-sdk-for-js/issues/2909
-            const requiredCORS = {
-                allowedHeaders: "*",
-                allowedMethods: "GET",
-                allowedOrigins: "localhost",
-                exposedHeaders: "*",
-                maxAgeInSeconds: 8888
-            };
-            if (!serviceProperties.cors) {
-                serviceProperties.cors = [requiredCORS];
-            }
-
-            //Post blob service properties back to Azure
-            await serviceURL.setProperties(Aborter.none, serviceProperties);
         } catch(error){
             this._logger.error('deployment failed: ' + error)
             throw error
         }
+    }
+
+    private async ConfigureDiagnosticSettings(params: any, util: any) {
+        let accountName: string;
+        let accountKey: string;
+
+        //Get blob storage properties
+        accountName = params["storageAccountName"].value;
+        const storageUtils = new StorageUtils(this._ctx);
+        accountKey = await storageUtils.get_primary_key(accountName, await util.resource_group())
+        const credentials = new SharedKeyCredential(accountName, accountKey);
+        const pipeline = StorageURL.newPipeline(credentials, {
+            // Enable logger when debugging
+            // logger: new ConsoleHttpPipelineLogger(HttpPipelineLogLevel.INFO)
+        });
+        const blobPrimaryURL = `https://${accountName}.blob.core.windows.net/`;
+        var serviceURL = new ServiceURL(blobPrimaryURL, pipeline)
+        const serviceProperties = await serviceURL.getProperties(Aborter.none);
+
+        //Get Bake variables for diagnostic settings.  Default to "true" (enabled) and 10 days data retention.
+        let blobDiagnosticHourlyMetricsEnabled: string = await util.variable("blobDiagnosticHourlyMetricsEnabled") || "true"
+        let blobDiagnosticHourlyMetricsRetentionDays = await util.variable("blobDiagnosticHourlyMetricsRetentionDays") || 10
+        let blobDiagnosticMinuteMetricsEnabled: string = await util.variable("blobDiagnosticMinuteMetricsEnabled") || "true"
+        let blobDiagnosticMinuteMetricsRetentionDays = await util.variable("blobDiagnosticMinuteMetricsRetentionDays") || 10
+        let blobDiagnosticLoggingEnabled: string = await util.variable("blobDiagnosticLoggingEnabled") || "true"
+        let blobDiagnosticLoggingRetentionDays = await util.variable("blobDiagnosticLoggingRetentionDays") || 10
+
+        //Workaround due to issues using boolean data type for Bake variables
+        var boolBlobDiagnosticHourlyMetricsEnabled: boolean = (blobDiagnosticHourlyMetricsEnabled == "true") ? true : false;
+        var boolBlobDiagnosticMinuteMetricsEnabled: boolean = (blobDiagnosticMinuteMetricsEnabled == "true") ? true : false;
+        var boolBlobDiagnosticLoggingEnabled: boolean = (blobDiagnosticLoggingEnabled == "true") ? true : false;
+
+        //Debug logging of Bake variables
+        this._logger.debug("blobDiagnosticHourlyMetricsEnabled:" + boolBlobDiagnosticHourlyMetricsEnabled)
+        this._logger.debug("blobDiagnosticHourlyMetricsRetentionDays:" + blobDiagnosticHourlyMetricsRetentionDays)
+        this._logger.debug("blobDiagnosticMinuteMetricsEnabled:" + boolBlobDiagnosticMinuteMetricsEnabled)
+        this._logger.debug("blobDiagnosticMinuteMetricsRetentionDays:" + blobDiagnosticMinuteMetricsRetentionDays)
+        this._logger.debug("blobDiagnosticLoggingEnabled:" + boolBlobDiagnosticLoggingEnabled)
+        this._logger.debug("blobDiagnosticLoggingRetentionDays:" + blobDiagnosticLoggingRetentionDays)
+
+        //Configure hourly metric settings
+        serviceProperties.hourMetrics = {
+            enabled: boolBlobDiagnosticHourlyMetricsEnabled,
+            retentionPolicy: {
+                days: blobDiagnosticHourlyMetricsRetentionDays,
+                enabled: true
+            },
+            version: "1.0"
+        };
+
+        //Azure will error if includeAPIs is set when the metrics diagnostics setting is not enabled
+        if (boolBlobDiagnosticHourlyMetricsEnabled) {
+            serviceProperties.hourMetrics.includeAPIs = true;
+        }
+
+        //Configure minute metric settings
+        serviceProperties.minuteMetrics = {
+            enabled: boolBlobDiagnosticMinuteMetricsEnabled,
+            retentionPolicy: {
+                days: blobDiagnosticMinuteMetricsRetentionDays,
+                enabled: true
+            },
+            version: "1.0"
+        };
+
+        //Azure will error if includeAPIs is set when the metrics diagnostics setting is not enabled
+        if (boolBlobDiagnosticMinuteMetricsEnabled) {
+            serviceProperties.minuteMetrics.includeAPIs = true;
+        }
+
+        //Configure logging settings
+        serviceProperties.logging = {
+            deleteProperty: boolBlobDiagnosticLoggingEnabled,
+            read: boolBlobDiagnosticLoggingEnabled,
+            retentionPolicy: {
+                days: blobDiagnosticLoggingRetentionDays,
+                enabled: true
+            },
+            version: "2.0",
+            write: boolBlobDiagnosticLoggingEnabled
+        };
+
+        //The Azure SDK will error with "storageServiceProperties.Cors must be of type Array." if no CORS value is specified.  Set to a restrictive setting.
+        //See: https://github.com/Azure/azure-sdk-for-js/issues/2909
+        const requiredCORS = {
+            allowedHeaders: "*",
+            allowedMethods: "GET",
+            allowedOrigins: "localhost",
+            exposedHeaders: "*",
+            maxAgeInSeconds: 8888
+        };
+        if (!serviceProperties.cors) {
+            serviceProperties.cors = [requiredCORS];
+        }
+
+        //Post blob service properties back to Azure
+        await serviceURL.setProperties(Aborter.none, serviceProperties);
     }
 }
