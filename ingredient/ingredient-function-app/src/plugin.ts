851970174339c28e524a81e0f386171fa99a96da
Kevin Reilly <kevinreilly@hchb.com>
2019-05-16 13:20:10 -0400

    adding arm-helper references and ingredient deployment code

--- ingredient/ingredient-function-app/src/plugin.ts
+++ ingredient/ingredient-function-app/src/plugin.ts
@@ -1,4 +1,6 @@
 import { BaseIngredient, IngredientManager } from "@azbake/core"
+import { ARMHelper } from "@azbake/arm-helper"
+import ARMTemplate from "./arm.json"
 
 export class FunctionAppPlugin extends BaseIngredient {
 
@@ -6,6 +8,13 @@ export class FunctionAppPlugin extends BaseIngredient {
         try {
             let util = IngredientManager.getIngredientFunction("coreutils", this._ctx)
             this._logger.log('Function App Plugin Logging: ' + this._ingredient.properties.source)
+
+            const helper = new ARMHelper(this._ctx);
+
+            let params = await helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters)
+
+            await helper.DeployTemplate(this._name, ARMTemplate, params, await util.resource_group())
+
         } catch(error){
             this._logger.error('deployment failed: ' + error)
             throw error
