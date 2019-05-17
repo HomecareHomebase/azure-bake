73cdbfb79a087ee81ce8ba9f0e9b194b48fed50d
Mo Cassidy <mcassidy@hchb.com>
2019-05-17 15:39:21 -0500

    KubeConfig from variable instead of file

--- ingredient/ingredient-kubernetes/src/plugin.ts
+++ ingredient/ingredient-kubernetes/src/plugin.ts
@@ -6,65 +6,85 @@ import Replace from 'replace-in-file';
 const exec = promisify(exec_from_child_process);
 
 export class KubernetesPlugin extends BaseIngredient {
-    
+
     public async Execute(): Promise<void> {
+        const kubeconfigFilename = 'kubeconfig.yaml'
         try {
-            let k8sYamlPath = await this._ingredient.properties.source.valueAsync(this._ctx);            
-            if (!await promisify(fs.exists)(k8sYamlPath)){
+            let k8sYamlPath = await this._ingredient.properties.source.valueAsync(this._ctx);
+            if (!await promisify(fs.exists)(k8sYamlPath)) {
                 throw "file/path not found: " + k8sYamlPath
             }
             await this.replaceTokens(k8sYamlPath);
             let testDeployment = this._ingredient.properties.parameters.get("testDeployment");
-            let k8sConfigPath = this._ingredient.properties.parameters.get("config");
-            let configParam = "";
-            if (k8sConfigPath){
-                configParam = `--kubeconfig=${await k8sConfigPath.valueAsync(this._ctx)}`;
-            }
-            let { stdout } = await exec(`kubectl apply ${configParam} -f ${k8sYamlPath}`);
-            this._logger.log(`stdout: ${stdout}`);
-            if (testDeployment && await testDeployment.valueAsync(this._ctx)){
-                ({ stdout } = await exec(`kubectl get ${configParam} services`));
+            let kubeConfigParam = await this.getKubeConfigParameter(kubeconfigFilename);
+            try {
+                let { stdout } = await exec(`kubectl apply ${kubeConfigParam} -f ${k8sYamlPath}`);
                 this._logger.log(`stdout: ${stdout}`);
-                ({ stdout } = await exec(`kubectl.exe delete ${configParam} -f ${k8sYamlPath}`));
-                this._logger.log(`stdout: ${stdout}`);
+                if (testDeployment && await testDeployment.valueAsync(this._ctx)) {
+                    ({ stdout } = await exec(`kubectl get ${kubeConfigParam} services`));
+                    this._logger.log(`stdout: ${stdout}`);
+                    ({ stdout } = await exec(`kubectl.exe delete ${kubeConfigParam} -f ${k8sYamlPath}`));
+                    this._logger.log(`stdout: ${stdout}`);
+                }
+            } finally {
+                if (kubeConfigParam) {
+                    try {
+                        await promisify(fs.unlink)(kubeconfigFilename)
+                    } catch (error) {
+                        this._logger.error(error)
+                    }
+                }
             }
-        } catch(error){
+        } catch (error) {
             this._logger.error('deployment failed: ' + error)
             throw error
         }
     }
-    private async replaceTokens(path: any): Promise<void>{
-        // TODO: If replacement goes global, instead of just k8s ingredient, then this next line seems bad
-        // const fileTypesFilter = ["yaml","yml","json"] // from https://kubernetes.io/docs/concepts/configuration/overview/
+    private async getKubeConfigParameter(kubeconfigFilename: string) {
+        let configParam = "";
+        let b64KubeConfigContent = this._ctx.Environment.variables.get("b64KubeConfigContent");
+        if (b64KubeConfigContent) {
+            let kubeConfigContent = Buffer.from(await b64KubeConfigContent.valueAsync(this._ctx), 'base64').toString('ascii');
+            this._ctx.Environment.variables.delete("b64KubeConfigContent");
+            try {
+                await promisify(fs.writeFile)(kubeconfigFilename, kubeConfigContent);
+            }
+            catch (error) {
+                this._logger.log(JSON.stringify(error, null, 2))
+            }
+            configParam = `--kubeconfig=${kubeconfigFilename}`
+        }
+        return configParam;
+    }
+    private async replaceTokens(path: any): Promise<void> {
         const openingDelimiter = "{{"
         const closingDelimiter = "}}"
-        console.log(path);        
-        if(await this.isDirectory(path)){
+        // If path is a directory, put a glob selector on it
+        if (await this.isDirectory(path)) {
             path = path + "/*"
         }
-        console.log(path);
-        const envKeys : RegExp[] = []
-        const envVals : string[] = []
 
-        let upperedVars : Map<string, BakeVariable> = new Map<string, BakeVariable>()
-        for (const [k,v] of this._ctx.Environment.variables) {
-            upperedVars.set(k.toUpperCase(),v)
+        const upperedVars: Map<string, BakeVariable> = new Map<string, BakeVariable>()
+        for (const [k, v] of this._ctx.Environment.variables) {
+            upperedVars.set(k.toUpperCase(), v)
         }
         // Merge tokens. Tokens win in cases of clashing keys
-        for (const [k,v] of this._ctx.Ingredient.properties.tokens) {
-            upperedVars.set(k.toUpperCase(),v)
+        for (const [k, v] of this._ctx.Ingredient.properties.tokens) {
+            upperedVars.set(k.toUpperCase(), v)
         }
-        for (const [k,v] of upperedVars) {
+        const envKeys: RegExp[] = []
+        const envVals: string[] = []
+        for (const [k, v] of upperedVars) {
             const value = await v.valueAsync(this._ctx)
-            envKeys.push(new RegExp(openingDelimiter+k+closingDelimiter,"g"))
+            envKeys.push(new RegExp(openingDelimiter + k + closingDelimiter, "g"))
             envVals.push(value || "")
         }
         const options = {
             files: path,
             from: envKeys,
             to: envVals,
-            // allowEmptyPaths: true,
-        };
+            allowEmptyPaths: true
+        }
         try {
             await Replace(options)
         }
@@ -72,7 +92,7 @@ export class KubernetesPlugin extends BaseIngredient {
             this._logger.error('Error occurred:', error)
         }
     }
-    private async isDirectory(path: any): Promise<boolean>{
+    private async isDirectory(path: any): Promise<boolean> {
         return (await promisify(fs.lstat)(path)).isDirectory()
     }
 }
