7251b78e4e5ad4ecd32c9cddd3939d4cc2afd35c
Kevin Reilly <kevinreilly@hchb.com>
2019-05-30 15:52:26 -0400

    added ability to parse resource group names out of resource names if provided

--- ingredient/ingredient-function-app/src/plugin.ts
+++ ingredient/ingredient-function-app/src/plugin.ts
@@ -6,18 +6,31 @@ export class FunctionAppPlugin extends BaseIngredient {
 
     public async Execute(): Promise<void> {
         try {
-            let util = IngredientManager.getIngredientFunction("coreutils", this._ctx)
-            this._logger.log('Function App Plugin Logging: ' + this._ingredient.properties.source)
+            let util = IngredientManager.getIngredientFunction("coreutils", this._ctx);
+            this._logger.log('Function App Plugin Logging: ' + this._ingredient.properties.source);
 
             const helper = new ARMHelper(this._ctx);
 
-            let params = await helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters)
+            let params = await helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters);
 
-            await helper.DeployTemplate(this._name, ARMTemplate, params, await util.resource_group())
+            //parse resource group names out of the resource names and populate the arm params accordingly
+            const hostResource =  util.parseResource(params["planName"].value);
+            const storageResource =  util.parseResource(params["storageAccountName"].value);
+            const aiResource =  util.parseResource(params["appInsightsName"].value);
+
+            params["planName"] = {"value": hostResource.resource};
+            params["planRG"] = {"value": (hostResource.resourceGroup || util.resource_group())};
+            params["storageAccountName"] = {"value": storageResource.resource};
+            params["storageAccountRG"] = {"value": (storageResource.resourceGroup || util.resource_group())};
+            params["appInsightsName"] = {"value": aiResource.resource};
+            params["appInsightsRG"] = {"value": (aiResource.resourceGroup || util.resource_group())};
+
+
+            await helper.DeployTemplate(this._name, ARMTemplate, params, await util.resource_group());
 
         } catch(error){
-            this._logger.error('deployment failed: ' + error)
-            throw error
+            this._logger.error('deployment failed: ' + error);
+            throw error;
         }
     }
 }
