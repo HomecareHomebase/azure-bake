d8d6daa4a7203c13ee4b6381c387465f95f7b783
bdschaap <bschaap@hchb.com>
2019-06-01 01:26:09 +0000

    fix: Feature/az mon unified diag settings1 (#61)

--- ingredient/ingredient-event-hub-namespace/src/functions.ts
+++ ingredient/ingredient-event-hub-namespace/src/functions.ts
@@ -2,11 +2,22 @@ import {BaseUtility, IngredientManager} from '@azbake/core'
 
 export class EventHubNamespaceUtils extends BaseUtility {
 
-    public create_resource_name(): string {
+    public get_resource_name(name:string): string {
         let util = IngredientManager.getIngredientFunction("coreutils", this.context)
 
-        const profile = util.create_resource_name("ehn", null, true);
-        return profile;
+        //resource type, name, region enabled
+        const fullName = util.create_resource_name("ehn", name, true);
+        return fullName;
     } 
+
+    public async get_resource_group(): Promise<string> {        
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+
+        let override = this.context.Config.rgOverride
+        if (override)
+            return await override.valueAsync(this.context);
+        //resource type, name, region enabled
+        else return util.create_resource_name("ehn", "", true);
+    }
 }
 
