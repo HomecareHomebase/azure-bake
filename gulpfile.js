84e6075dbdbb8c95afd8bf96ab8e6e7a7805baf9
whilke <whilke@gmail.com>
2019-06-17 17:40:13 -0500

    chore: fixing order of commit->publish

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ function build(done) {
         if ((!!params.build.buildReason.match(/IndividualCI/ig) || !!params.build.buildReason.match(/BatchedCI/ig)) &&
             !!params.build.buildSourceBranch.replace(/refs\/heads\/(feature\/)?/i, '').match(/master/ig)) {
             console.log('Running Azure DevOps Release Build');
-            gulp.series(printVersion, adoPrep, toolInstall, lernaBuild, lernaPublish, systemPublish, gitCommit )(done);
+            gulp.series(printVersion, adoPrep, toolInstall, lernaBuild, gitCommit, lernaPublish, systemPublish )(done);
         }
 
         else if (!!params.build.buildReason.match(/PullRequest/ig)) {
