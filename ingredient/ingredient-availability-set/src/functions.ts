aaed7892dc2e0d87f7dd59fefb2e41441162b227
Kevin Reilly <kevinreilly@hchb.com>
2019-06-19 17:46:38 -0400

    adding functions to retrieve fault domain and update domain counts

--- ingredient/ingredient-availability-set/src/functions.ts
+++ ingredient/ingredient-availability-set/src/functions.ts
@@ -9,5 +9,25 @@ export class AvailabilitySetUtils extends BaseUtility {
         const name = util.create_resource_name("avail", null, true);
         return name;
     }
+
+    public async get_fault_domain_count(): Promise<number> {
+        //asdf
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context);
+        let client = new ResourceManagementClient(this.context.AuthToken, this.context.Environment.authentication.subscriptionId);
+
+        let availResource = await client.resources.get(await util.resource_group(), "Microsoft.Compute", "", "availabilitySets", name, "2018-06-01");
+
+        return availResource.properties.platformFaultDomainCount;
+    }
+
+    public async get_update_domain_count(): Promise<number> {
+        //asdf
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context);
+        let client = new ResourceManagementClient(this.context.AuthToken, this.context.Environment.authentication.subscriptionId);
+
+        let availResource = await client.resources.get(await util.resource_group(), "Microsoft.Compute", "", "availabilitySets", name, "2018-06-01");
+
+        return availResource.properties.platformUpdateDomainCount;
+    }
 }
 
