8b41192dcee5883cec745d42a9d5639c96b28c90
Brian Schaap <bschaap@hchb.com>
2019-06-25 17:13:22 -0400

    configured Service Bus diagnostic settings

--- ingredient/ingredient-service-bus-namespace/src/plugin.ts
+++ ingredient/ingredient-service-bus-namespace/src/plugin.ts
@@ -1,6 +1,8 @@
 import { BaseIngredient, IngredientManager } from "@azbake/core"
 import { ARMHelper } from "@azbake/arm-helper"
 import ARMTemplate from "./arm.json"
+import { EventHubNamespaceUtils } from "@azbake/ingredient-event-hub-namespace"
+
 export class ServiceBusNamespace extends BaseIngredient {
 
     public async Execute(): Promise<void> {
@@ -11,6 +13,22 @@ export class ServiceBusNamespace extends BaseIngredient {
             const helper = new ARMHelper(this._ctx);
 
             let params = await helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters)
+
+            if (!params["diagnosticsEnabled"])
+                params["diagnosticsEnabled"] = {"value": "yes"}
+
+            if (params["diagnosticsEnabled"].value == "yes") {
+                const ehnUtils = new EventHubNamespaceUtils(this._ctx);
+
+                var diagnosticsEventHubNamespace = ehnUtils.get_resource_name("diagnostics");
+                params["diagnosticsEventHubNamespace"] = {"value": diagnosticsEventHubNamespace};
+              
+                var diagnosticsEventHubResourceGroup: string
+
+                diagnosticsEventHubResourceGroup = await ehnUtils.get_resource_group();
+
+                params["diagnosticsEventHubResourceGroup"] = {"value": diagnosticsEventHubResourceGroup};                
+            }
 
             await helper.DeployTemplate(this._name, ARMTemplate, params, await util.resource_group())
         } catch(error){
