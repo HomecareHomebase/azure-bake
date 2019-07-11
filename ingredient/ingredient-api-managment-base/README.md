b3c426c83263f147b9053a42454acbd286d6ceca
Charlie Sperbeck <csperbeck@hchb.com>
2019-07-11 05:37:59 -0400

    allow multiple loggers on apim

--- ingredient/ingredient-api-managment-base/README.md
+++ ingredient/ingredient-api-managment-base/README.md
@@ -17,39 +17,136 @@ resourceGroup: true
 rgOverride: test
 parallelRegions: false
 recipe:
-  linux-test:
+  apim-test:
     properties:
-      type: "@azbake/ingredient-api-management-api"
+      type: "@azbake/ingredient-api-management-base"
       parameters:
-        apimInstance: "testapim"
-        apiName: "testapi"
-        backend: "testbackend"
+        apiManagementServiceName: charlietestapim
+        publisherEmail: john.smith@contoso.com
+        publisherName: John Smith
+        virtualNetworkName: testvnet
+        virtualNetworkResourceGroupName: test
+        SubnetName: testsubnet
+        properties:
+          foo:
+            key: bar
+            isSecret: true
+            tags:
+              - test
+              - dev
+        logger:
+          testai:
+            resourceGroup: test
+            type: applicationInsights
+            clean: true
+          testai2:
+            resourceGroup: test
+            type: applicationInsights
 ```
 
 ##Properties
-Set: 
-| property | value |
-| osType | windows |
+| property | required | description |
+| -------- | -------- | ----------- |
+| apiManagementServiceName | yes | Name for the API Management instance |
+| publisherEmail | yes | Email for the primary API publisher |
+| publisherName | yes | Name for the primary API publisher |
+| virtualNetworkName | yes | Primary vNet connection |
+| virtualNetworkResourceGroupName | yes | Resource group for the primary vNet |
+| subnetName | yes | Name of the subnet in your vNet |
+| properties | no | Array of objects for named key/value pairs |
+| logger | no | Object for the connection of an Application Insights or EventHub Logger |
+
+###Named Values
+| property | required | description |
+| -------- | -------- | ----------- |
+| key | yes | Value to be stored with the key name |
+| isSecret | no | Sets value to be encrypted | 
+| tags | no | Array of tag values to be set |
+
+###Logger
+| property | required | description |
+| -------- | -------- | ----------- |
+| type | yes | Specifies an ApplicationInsights or EventHub Logger |
+| resourceGroup | no | Resource group that the logger is located in |
+| clean | no | Cleans up old keys for the logger instance specified. Defaults to `true` |
 
 ## Utilities
 
 Utility classes can be used inside of the bake.yaml file for parameter and source values.
 
-### ``apim-api`` class
+### ``apim-base`` class
 
 | function | description |
-| `create_resource_name()` | Returns the name created for the Service Bus Namespace when deployed |
+| `create_resource_name()` | Returns the name created for the API Management Instance when deployed |
+| `get_logger(name: string, rg: string, match: string)` | Returns the logger name containing the match string |
+| `get_property(name: string, rg: string, match: string)` | Returns the value of the key/value pair of the name in the 
+match string |
+| `get_api(name: string, rg: string, match: string)` | Returns the value of the API name in the match string |
+| `get_backend(name: string, rg: string, match: string)` | Returns the value of the backend in the match string |
 
 ### Function Details
 
 #### create_resource_name()
 
-Gets the name created for the App Service Plan when deployed.
+Gets the name created for the API Management Instance when deployed.
 
 ```yaml
 ...
 parameters:
-    planName: "[apim-api.create_resource_name()]"
+    name: "[apim-base.create_resource_name()]"
+...
+```
+##### Returns
+
+string
+
+### get_logger(name: string, rg: string, match: string)
+Returns the logger name containing the match string. For example if you specify `DEVAI` it will find all the loggers attached to the instance that container that match in their name.
+
+```yaml
+...
+parameters:
+    logger: "[apim-base.get_logger("testapim","test","devai")]"
+...
+```
+##### Returns
+
+string
+
+### get_property(name: string, rg: string, match: string)
+Returns the property value containing for the key containing match string. For example if you specify `DEVCONN` it will find the key attached to the instance that contains that match.
+
+```yaml
+...
+parameters:
+    property: "[apim-base.get_property("testapim","test","devconnstr")]"
+...
+```
+##### Returns
+
+string
+
+### get_api(name: string, rg: string, match: string)
+Returns the name of the API containing the match string.
+
+```yaml
+...
+parameters:
+    api: "[apim-base.get_api("testapim","test","testapi")]"
+...
+```
+
+##### Returns
+
+string
+
+### get_backend(name: string, rg: string, match: string)
+Returns the name of the Backend containing the match string.
+
+```yaml
+...
+parameters:
+    backend: "[apim-base.get_property("testapim","test","testbackend")]"
 ...
 ```
 
--- ingredient/ingredient-api-managment-base/src/plugin.ts
+++ ingredient/ingredient-api-managment-base/src/plugin.ts
@@ -11,7 +11,7 @@ export class ApimBase extends BaseIngredient {
         try {
             this._logger.log(`API Manamgement: Base Logging - ${this._ingredient.properties.source}`)
             let aiClient = new ApplicationInsightsManagementClient(this._ctx.AuthToken, this._ctx.Environment.authentication.subscriptionId);
-            let client = new ApiManagementClient(this._ctx.AuthToken, this._ctx.Environment.authentication.subscriptionId)    
+            let client = new ApiManagementClient(this._ctx.AuthToken, this._ctx.Environment.authentication.subscriptionId)
             let util = IngredientManager.getIngredientFunction("coreutils", this._ctx)
             const helper = new ARMHelper(this._ctx);
             let params = await helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters)
@@ -19,7 +19,7 @@ export class ApimBase extends BaseIngredient {
             //Import deployment parameters
             let properties = params["properties"]
             delete params["properties"]
-            let loggerProps = params["logger"]            
+            let loggerProps = params["logger"]
             delete params["logger"]
             //Deploy primary ARM template
             await helper.DeployTemplate(this._name, ApimTemplate, params, await util.resource_group())
@@ -53,60 +53,65 @@ export class ApimBase extends BaseIngredient {
                         }
                     });
                 }
-            }
+            }           
 
             //Create Logger Connection to Application Insights
             if (loggerProps) {
-                let aiRg = loggerProps.value.resourceGroup || await util.resource_group()
-                let apimRg = await util.resource_group() || ""
-                let aiName = loggerProps.value.name || ""
-
-                let currentLoggerCreds: any
-                if (loggerProps.value.type == "applicationInsights") {
-                    this._logger.log(`logger: Getting instrumentation key from '${aiName}' in resource group '${aiRg}'`)
-                    let response = await aiClient.components.get(aiRg, aiName)
-                    let aiKey: string = ""
-                    if (response.instrumentationKey) {
-                        aiKey = response.instrumentationKey || ""
-                    }
-                    await client.logger.createOrUpdate(apimRg, serviceName, aiName, {
-                        credentials: { instrumentationKey: aiKey },
-                        loggerType: 'applicationInsights'
-                    }).then((result) => {
-                        if (result.eTag && result.name == aiName && result.credentials.instrumentationKey && result.loggerType == loggerProps.value.type) {
-                            this._logger.log(`logger: Deployed ${result.loggerType} connection for '${aiName}' on '${serviceName}'`)
-                            currentLoggerCreds = result.credentials.instrumentationKey.replace(/{{|}}/ig, "")
+                let keys = Object.keys(loggerProps.value)
+                for (let i = 0; i < keys.length; i++) {
+                    let item = keys[i]
+                    let loggerVal = loggerProps.value[item] || ""                    
+                    let aiRg = loggerVal.resourceGroup || await util.resource_group()
+                    let apimRg = await util.resource_group() || ""
+                    let aiName = item || ""
+                    let currentLoggerCreds: any                    
+                    if (loggerVal.type == "applicationInsights") {
+                        this._logger.log(`logger: Getting instrumentation key from '${aiName}' in resource group '${aiRg}'`)
+                        let response = await aiClient.components.get(aiRg, aiName)
+                        let aiKey: string = ""
+                        if (response.instrumentationKey) {
+                            aiKey = response.instrumentationKey || ""
                         }
-                        else {
-                            throw `logger: ${aiName} was not deployed correctly`
-                        }
-                    });
-                    //Clean logger keys
-                    if (loggerProps.value.clean) {
-                        let result = await client.property.listByService(apimRg, serviceName) || ""                        
-                        let propEtag = ""
-                        for (let i = 0; i < result.length; i++) {
-                            let id = result[i].name || ""
-                            let displayName = result[i].displayName || ""
-                            let currentLogKey = ""
-                            let propTags = result[i].tags || []
-                            if (displayName != currentLoggerCreds && displayName.match(/Logger.Credentials-.*/) && result[i].value == aiKey) {
-                                await client.property.getEntityTag(apimRg, serviceName, id).then((result) => { propEtag = result.eTag })
-                                await client.property.deleteMethod(apimRg, serviceName, id, propEtag)
-                                    .then((result) => {
-                                        this._logger.log(`logger: Removed old key - ${displayName}: ${result._response.status == 200}`)
-                                    })
-                                    .catch((failure) => {
-                                        this._logger.log(`logger: failed to remove AppInsights key: ${displayName}`)
-                                    })
+                        await client.logger.createOrUpdate(apimRg, serviceName, aiName, {
+                            credentials: { instrumentationKey: aiKey },
+                            loggerType: 'applicationInsights'
+                        }).then((result) => {
+                            if (result.eTag && result.name == aiName && result.credentials.instrumentationKey && result.loggerType == loggerVal.type) {
+                                this._logger.log(`logger: Deployed ${result.loggerType} connection for '${aiName}' on '${serviceName}'`)
+                                currentLoggerCreds = result.credentials.instrumentationKey.replace(/{{|}}/ig, "")
+                            }
+                            else {
+                                throw `logger: ${aiName} was not deployed correctly`
+                            }
+                        });
+                        //Clean logger keys
+                        if (loggerVal.clean == undefined || loggerVal.value.clean) {
+                            let result = await client.property.listByService(apimRg, serviceName) || ""
+                            let propEtag = ""
+                            for (let i = 0; i < result.length; i++) {
+                                let id = result[i].name || ""
+                                let displayName = result[i].displayName || ""
+                                let currentLogKey = ""
+                                let propTags = result[i].tags || []
+                                if (displayName != currentLoggerCreds && displayName.match(/Logger.Credentials-.*/) && result[i].value == aiKey) {
+                                    await client.property.getEntityTag(apimRg, serviceName, id).then((result) => { propEtag = result.eTag })
+                                    await client.property.deleteMethod(apimRg, serviceName, id, propEtag)
+                                        .then((result) => {
+                                            this._logger.log(`logger: Removed old key - ${displayName}: ${result._response.status == 200}`)
+                                        })
+                                        .catch((failure) => {
+                                            this._logger.log(`logger: failed to remove AppInsights key: ${displayName}`)
+                                        })
+                                }
                             }
                         }
                     }
+                    else if (loggerVal.value.type == "azureEventHub") {
+                        this._logger.log(`Logger: EventHub functionality is yet to be implemented`)
+                    }
                 }
             }
-            else if (loggerProps.value.type == "azureEventHub") {
-                this._logger.log(`Logger: EventHub functionality is yet to be implemented`)
-            }
+
         } catch (error) {
             this._logger.error('deployment failed: ' + error)
             throw error
