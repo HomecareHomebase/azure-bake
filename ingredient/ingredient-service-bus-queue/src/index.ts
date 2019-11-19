6911e7bfc95bb88eae90e27e7b67b69ea90af49d
Konstantin Nikitenko <knikitenko@hchb.com>
2019-11-18 21:50:16 -0600

    added ingredient-service-bus-queue

---
+++ ingredient/ingredient-service-bus-queue/src/index.ts
@@ -0,0 +1,10 @@
+import { ServiceBusQueuePlugin} from './plugin'
+import { ServiceBusQueueUtils} from './functions'
+
+/*  comment out these entries if you are not including an ingredient plugin runner*/
+exports.plugin = ServiceBusQueuePlugin
+exports.pluginNS = "@azbake/ingredient-service-bus-queue" //name of the ingredient to reference in a bake.yaml recipe
+
+/* comment out these entries if you are not including a set of expression functions*/
+exports.functions = ServiceBusQueueUtils
+exports.functionsNS = "servicebusqueue" //bake.yaml expressions can access your functions via "myutils.my_function()"
