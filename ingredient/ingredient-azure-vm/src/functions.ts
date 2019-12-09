a754b5c94f823897fd1462d6dbc03ca9fbc402ba
Adhithan Veeraraghavan <aveeraraghavan@hchb.com>
2019-12-09 06:37:23 -0600

    added shortName to create resource name

--- ingredient/ingredient-azure-vm/src/functions.ts
+++ ingredient/ingredient-azure-vm/src/functions.ts
@@ -2,9 +2,9 @@ import {BaseUtility, IngredientManager} from '@azbake/core'
 
 export class AzureVmUtils extends BaseUtility {
 
-    public create_resource_name(): string {
+    public create_resource_name(shortName: string): string {
         let util = IngredientManager.getIngredientFunction("coreutils", this.context);
-        const name = util.create_resource_name("vm", null, false);
+        const name = util.create_resource_name("vm", shortName, false);
         return name;
     }
 }
