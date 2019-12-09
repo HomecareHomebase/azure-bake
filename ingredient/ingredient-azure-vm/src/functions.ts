e1136d9d330377e4f2bc5724791d5fd36a6b3bc1
Adhithan Veeraraghavan <aveeraraghavan@hchb.com>
2019-12-09 10:48:29 -0600

    Changed shortName as optional parameter

--- ingredient/ingredient-azure-vm/src/functions.ts
+++ ingredient/ingredient-azure-vm/src/functions.ts
@@ -2,9 +2,10 @@ import {BaseUtility, IngredientManager} from '@azbake/core'
 
 export class AzureVmUtils extends BaseUtility {
 
-    public create_resource_name(shortName: string): string {
+    public create_resource_name(shortName?: string): string {
         let util = IngredientManager.getIngredientFunction("coreutils", this.context);
-        const name = util.create_resource_name("vm", shortName, false);
+        const name = util.create_resource_name("vm", (shortName != undefined) ? shortName : null, false);
         return name;
     }
+   
 }
