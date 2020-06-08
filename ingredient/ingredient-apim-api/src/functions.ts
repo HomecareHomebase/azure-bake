dae4f305fa6abb964d445bb534712f8a8860bfa9
Josh Brown <joshbrown@hchb.com>
2020-06-07 20:11:58 -0400

    remove unused imports

--- ingredient/ingredient-apim-api/src/functions.ts
+++ ingredient/ingredient-apim-api/src/functions.ts
@@ -1,6 +1,4 @@
 import {BaseUtility, IngredientManager} from '@azbake/core'
-import { ApiManagementClient } from "@azure/arm-apimanagement"
-import { ApplicationInsightsManagementClient } from '@azure/arm-appinsights'
 
 export class ApimApiUtils extends BaseUtility {
 }
