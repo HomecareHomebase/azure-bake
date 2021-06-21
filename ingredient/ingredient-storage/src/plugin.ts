8d3edf61190e013b53f3f7a151962ad2bf90d28b
Josh Brown <joshbrown@hchb.com>
2021-06-21 11:30:34 -0400

    upload to storage by file and directory

--- ingredient/ingredient-storage/src/plugin.ts
+++ ingredient/ingredient-storage/src/plugin.ts
@@ -4,10 +4,10 @@ import { ARMHelper } from "@azbake/arm-helper"
 import ARMTemplate from "./storage.json"
 import stockAlerts from "./stockAlerts.json"
 import { StorageUtils } from "./functions.js";
-import { StorageManagementClient } from "@azure/arm-storage"
-import { StorageSharedKeyCredential, BlobServiceClient } from "@azure/storage-blob"
+import { StorageSharedKeyCredential, BlobServiceClient, ContainerClient } from "@azure/storage-blob"
 import ARMTemplateNetwork from "./storageNetwork.json"
 import ARMTemplateDataLake from "./storageDatalake.json"
+import * as fs from 'fs';
 
 export class StoragePlugIn extends BaseIngredient {
     public async Execute(): Promise<void> {
@@ -19,48 +19,81 @@ export class StoragePlugIn extends BaseIngredient {
             
             let params = await helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters)
             
-            if(params['NetworkAcls']){
-
-                await helper.DeployTemplate(this._name, ARMTemplateNetwork, params, await util.resource_group())
-                //there is a limitation around the copy function in the current architecture
-
-            }else if(params['IsHnsEnabled'])
+            if(params['deploy'] == undefined || params['deploy'].value) 
             {
-                await helper.DeployTemplate(this._name, ARMTemplateDataLake, params, await util.resource_group())
-            }
-            else
-            {
-                await helper.DeployTemplate(this._name, ARMTemplate, params, await util.resource_group())
+                if(params['NetworkAcls'])
+                {
+                    await helper.DeployTemplate(this._name, ARMTemplateNetwork, params, await util.resource_group())
+                    //there is a limitation around the copy function in the current architecture
+                }
+                else if(params['IsHnsEnabled'])
+                {
+                    await helper.DeployTemplate(this._name, ARMTemplateDataLake, params, await util.resource_group())
+                }
+                else
+                {
+                    await helper.DeployTemplate(this._name, ARMTemplate, params, await util.resource_group())
+                }
+    
+                try 
+                {
+                    await this.ConfigureDiagnosticSettings(params, util);                
+                }
+                catch (diagError) {
+                    this._logger.debug('diag error: ' + diagError) //some storage types don't support diag settings
+                }
+    
+                let alertTarget = params["storageAccountName"].value
+                let alertOverrides = this._ingredient.properties.alerts
+                await helper.DeployAlerts(this._name, await util.resource_group(), alertTarget, stockAlerts, alertOverrides)
             }
 
-            try {
-                await this.ConfigureDiagnosticSettings(params, util);                
-            }
-            catch (diagError){
-                this._logger.debug('diag error: ' + diagError) //some storage types don't support diag settings
-            }
+            let source = await this._ingredient.properties.source.valueAsync(this._ctx);
 
-            let alertTarget = params["storageAccountName"].value
-            let alertOverrides = this._ingredient.properties.alerts
-            await helper.DeployAlerts(this._name, await util.resource_group(), alertTarget, stockAlerts, alertOverrides)
+            if (source) {
+                await this.DeploySource(source, params, util);
+            }
         } catch(error){
             this._logger.error('deployment failed: ' + error)
             throw error
         }
     }
 
+    private async DeploySource(source: any, params: any, util: any) {
+        
+        this._logger.log(`Beginning source upload to storage`);
+
+        if(params['container'] == undefined)
+        {
+            this._logger.error(`Container parameter not specified`);
+            return;
+        }
+
+        if(params['uploadPath'] == undefined)
+        {
+            this._logger.error(`Upload Path parameter not specified`);
+            return;
+        }
+
+        const blobClient = await this.GetBlobServiceClient(params, util);
+        const containerClient = blobClient.getContainerClient(params['container'].value);
+
+        // upload single file
+        if(source.startsWith("file:///")) {
+            var filePath = source.replace("file:///", "");
+
+            await this.UploadSingleBlob(containerClient, filePath, params);
+        }
+        // upload directory
+        else {
+            for (const fileName of fs.readdirSync(source)) {
+                await this.UploadSingleBlob(containerClient, `${source}/${fileName}`, params); 
+            }
+        }    
+    }
+
     private async ConfigureDiagnosticSettings(params: any, util: any) {
-        let accountName: string;
-        let accountKey: string;
-
-        //Get blob storage properties
-        accountName = params["storageAccountName"].value;
-        const storageUtils = new StorageUtils(this._ctx);
-        accountKey = await storageUtils.get_primary_key(accountName, await util.resource_group())
-        const credentials = new StorageSharedKeyCredential(accountName, accountKey);
-        const blobPrimaryURL = `https://${accountName}.blob.core.windows.net/`;
-
-        const blobClient = new BlobServiceClient(blobPrimaryURL, credentials);
+        const blobClient = await this.GetBlobServiceClient(params, util);
         const serviceProperties = await blobClient.getProperties()
 
         //Get Bake variables for diagnostic settings.  Default to "true" (enabled) and 10 days data retention.
@@ -133,5 +166,30 @@ export class StoragePlugIn extends BaseIngredient {
 
         //Post blob service properties back to Azure
         await blobClient.setProperties(serviceProperties)
+    }
+
+    private async GetBlobServiceClient(params: any, util: any): Promise<BlobServiceClient> {
+        let accountName: string;
+        let accountKey: string;
+
+        accountName = params["storageAccountName"].value;
+        const storageUtils = new StorageUtils(this._ctx);
+        accountKey = await storageUtils.get_primary_key(accountName, await util.resource_group())
+        const credentials = new StorageSharedKeyCredential(accountName, accountKey);
+        const blobPrimaryURL = `https://${accountName}.blob.core.windows.net/`;
+
+        return new BlobServiceClient(blobPrimaryURL, credentials);
+    }
+
+    private async UploadSingleBlob(containerClient: ContainerClient, filePath: string, params: any) {
+        var path = require("path");
+
+        let fileName = path.basename(filePath);
+        const blobName = `${params['uploadPath'].value}/${fileName}`;
+
+        const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+        const uploadBlobResponse = await blockBlobClient.uploadFile(filePath);
+        this._logger.log(`Upload blob "${fileName}" successfully`, uploadBlobResponse.requestId)
     }
 }
