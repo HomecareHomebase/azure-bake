bacb5565a4deb89772b76bb5cf6e392937ece457
Darrell Swiney <dswiney@hchb.com>
2022-01-14 17:20:08 -0500

    initial add of ingredient

---
+++ ingredient/ingredient-batch/src/functions.ts
@@ -0,0 +1,12 @@
+import {BaseUtility, IngredientManager} from '@azbake/core'
+
+export class BatchUtils extends BaseUtility {
+
+    public create_resource_name(): string {
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context);
+
+        const name = util.create_resource_name("batch", null, true);
+        return name;
+    }
+}
+
---
+++ ingredient/ingredient-batch/src/plugin.ts
@@ -0,0 +1,31 @@
+import { BaseIngredient,IngredientManager,IIngredient,DeploymentContext } from "@azbake/core"
+import { ARMHelper } from "@azbake/arm-helper"
+import ARMTemplate from "./batch.json"
+
+export class BatchPlugin extends BaseIngredient {
+
+    constructor(name: string, ingredient: IIngredient, ctx: DeploymentContext) {
+        super(name, ingredient, ctx);
+        this._helper = new ARMHelper(this._ctx);
+    }
+    
+    _helper: ARMHelper;
+
+    public async Execute(): Promise<void> {
+        try {
+            let util = IngredientManager.getIngredientFunction("coreutils", this._ctx)
+            
+            this._logger.log('DataBricks Logging: ' + this._ctx.Ingredient.properties.source)
+
+            let params = await this._helper.BakeParamsToARMParamsAsync(this._name, this._ctx.Ingredient.properties.parameters)
+            
+            params = await this._helper.ConfigureDiagnostics(params);
+            
+            await this._helper.DeployTemplate(this._name, ARMTemplate, params, await util.resource_group())            
+                        
+        } catch(error){
+            this._logger.error('deployment failed: ' + error)
+            throw error
+        }
+    }
+}
