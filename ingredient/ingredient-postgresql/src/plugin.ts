dc2eed0cd0af642e9d790199ee9ad2491900a8b4
Nick Marinelli <nmarinelli@hchb.com>
2022-04-07 16:51:55 -0400

    switch ARM templates based on access parameter

--- ingredient/ingredient-postgresql/src/plugin.ts
+++ ingredient/ingredient-postgresql/src/plugin.ts
@@ -1,6 +1,7 @@
 import { BaseIngredient, IngredientManager,IIngredient,DeploymentContext } from "@azbake/core"
 import { ARMHelper } from "@azbake/arm-helper"
-import ARMTemplate from "./FlexibleServerArm.json" 
+import PublicAccessARMTemplate from "./PublicAccessArm.json" 
+import PrivateAccessARMTemplate from "./PrivateAccessArm.json"
 
 export class PostgreSQLDB extends BaseIngredient {
 
@@ -13,15 +14,26 @@ export class PostgreSQLDB extends BaseIngredient {
 
     public async Execute(): Promise<void> {
         try {
+            var params = await this._helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters)
+
+            var ARMTemplate = (params.access.value == "public") ? PublicAccessARMTemplate
+                : (params.access.value == "private") ? PrivateAccessARMTemplate
+                : null;
+            
+            if (ARMTemplate == null) throw new Error("Parameter 'access' must be set to \"public\" or \"private\".");
+        } catch (error){
+            this._logger.error('Bake validation failed: ' + error)
+            throw error;
+        }
+
+        try {
             let util = IngredientManager.getIngredientFunction("coreutils", this._ctx)
             this._logger.log('PostgreSQL Plugin Logging: ' + this._ingredient.properties.parameters)
-
-            let params = await this._helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters)
 
             await this._helper.DeployTemplate(this._name, ARMTemplate, params, await util.resource_group())
 
         } catch(error){
-            this._logger.error('deployment failed: ' + error)
+            this._logger.error('Deployment failed: ' + error)
             throw error
         }
     }
