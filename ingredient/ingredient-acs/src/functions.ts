aac452287cd60de1474e7cdea05c97707d642b94
Larry Richardson <lrichardson@hchb.com>
2023-01-16 14:29:37 -0500

    Build works

--- ingredient/ingredient-acs/src/functions.ts
+++ ingredient/ingredient-acs/src/functions.ts
@@ -1,8 +1,5 @@
 import {BaseUtility, IngredientManager} from '@azbake/core'
 import {CommunicationServiceManagementClient} from '@azure/arm-communication'
-import { ARMHelper } from "@azbake/arm-helper"
-import ARMTemplate from "./arm.json"
-
 export class AcsUtils extends BaseUtility {
 
 
@@ -12,59 +9,25 @@ export class AcsUtils extends BaseUtility {
         return st_profile;
     }
 
-    public async get_primary_key(name: string, rg: string | null = null) : Promise<string> {
-     
-        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
-        let resource_group = rg || await util.resource_group()
-
-        const client = new CommunicationServiceManagementClient(this.context.AuthToken, this.context.Environment.authentication.subscriptionId);
-
-        let response = await client.storageAccounts.listKeys(resource_group, name)
-
-        let key: string = ""
-        if (response.keys)
-        {
-            key = response.keys[0].value || ""
-        }
-        return key
-    }
-
-    public async get_secondary_key(name: string, rg: string | null = null) : Promise<string> {
-     
-        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
-        let resource_group = rg || await util.resource_group()
-
-        const client = new CommunicationServiceManagementClient(this.context.AuthToken, this.context.Environment.authentication.subscriptionId);
-
-        let response = await client.storageAccounts.listKeys(resource_group, name)
-
-        let key: string = ""
-        if (response.keys)
-        {
-            key = response.keys[1].value || ""
-        }
-        return key
-    }
-
     public async get_primary_connectionstring(name: string, rg: string | null = null) : Promise<string> {
-        let key: string = ""
+     
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        let resource_group = rg || await util.resource_group()
 
-        key = await this.get_primary_key(name, rg);
-    
-
-        let connectionString = `DefaultEndpointsProtocol=https;AccountName=${name};AccountKey=${key};`        
+        // The communication client needs a token credential, instead of the auth. https://learn.microsoft.com/en-us/java/api/com.azure.core.credential.tokencredential?source=recommendations&view=azure-java-stable
         
-        return connectionString;
-    }
+        const client = new CommunicationServiceManagementClient(this.context.AuthToken, this.context.Environment.authentication.subscriptionId);
 
-    public async get_secondary_connectionstring(name: string, rg: string | null = null) : Promise<string> {
+        let response = await client.communicationService.listKeys(resource_group, name)
+
         let key: string = ""
-
-        key = await this.get_secondary_key(name, rg);
-
-        let connectionString = `DefaultEndpointsProtocol=https;AccountName=${name};AccountKey=${key};`        
-        
-        return connectionString;        
+        if (response.primaryConnectionString)
+        {
+            return response.primaryConnectionString
+        }
+        return key
     }
+ 
+
 }
 
