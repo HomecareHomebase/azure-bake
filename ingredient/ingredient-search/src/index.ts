f2f43644694595b4969b276b849d4e4f6908df32
Josh Brown <joshbrown@hchb.com>
2025-01-29 12:09:24 -0500

    feat: add search ingredient

--- ingredient/ingredient-search/src/index.ts
+++ ingredient/ingredient-search/src/index.ts
@@ -1,7 +1,8 @@
+import { SearchPlugIn } from './plugin'
 import { SearchUtils } from './functions'
 
-exports.plugin = null
-exports.pluginNS = null
+exports.plugin = SearchPlugIn
+exports.pluginNS = "@azbake/ingredient-search"
 
 exports.functions = SearchUtils
 exports.functionsNS = "search"
