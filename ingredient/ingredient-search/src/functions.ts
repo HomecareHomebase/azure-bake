fe4477b9d8f77d0aa7ca5e650cb1556560bb9cec
Josh Brown <mtyrealhanla@users.noreply.github.com>
2025-03-05 17:11:25 -0500

    feat: add azure search ingredient (#312)
    * feat: initial add of search ingredient
    
    * feat: add search ingredient
    
    ---------
    
    Co-authored-by: Josh Brown <joshbrown@hchb.com>

---
+++ ingredient/ingredient-search/src/functions.ts
@@ -0,0 +1,54 @@
+import { BaseUtility, IngredientManager } from '@azbake/core'
+import { ClientSecretCredential } from '@azure/identity';
+import { SearchManagementClient } from '@azure/arm-search'
+
+export class SearchUtils extends BaseUtility {
+
+    public create_resource_name(): string {
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        const ais_profile = util.create_resource_name("ais", null, false);
+        return ais_profile;
+    }
+
+    public async get_primary_admin_key(name: string, rg: string | null = null) : Promise<string> {
+     
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        let resource_group = rg || await util.resource_group()
+
+        const token = new ClientSecretCredential(this.context.AuthToken.domain, this.context.AuthToken.clientId, this.context.AuthToken.secret);
+
+        const client = new SearchManagementClient(token, this.context.Environment.authentication.subscriptionId);
+
+        let response = await client.adminKeys.get(resource_group, name)
+
+        let key: string = ""
+
+        if (response)
+        {
+            key = response.primaryKey || ""
+        }
+
+        return key
+    }
+
+    public async get_secondary_admin_key(name: string, rg: string | null = null) : Promise<string> {
+     
+        let util = IngredientManager.getIngredientFunction("coreutils", this.context)
+        let resource_group = rg || await util.resource_group()
+
+        const token = new ClientSecretCredential(this.context.AuthToken.domain, this.context.AuthToken.clientId, this.context.AuthToken.secret);
+
+        const client = new SearchManagementClient(token, this.context.Environment.authentication.subscriptionId);
+
+        let response = await client.adminKeys.get(resource_group, name)
+
+        let key: string = ""
+
+        if (response)
+        {
+            key = response.secondaryKey || ""
+        }
+        
+        return key
+    }
+}
