ac859de97209b0107efdfe0af1ecf74cfb5fe02c
Bill <whilke@gmail.com>
2025-11-07 00:27:56 -0600

    FIX: Refactor crypto shim and update exports syntax

--- ingredient/ingredient-apim-api/src/index.ts
+++ ingredient/ingredient-apim-api/src/index.ts
@@ -1,3 +1,6 @@
+// index.ts
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 try {
   const { webcrypto } = require('crypto'); // Node's Web Crypto (Node 16+: experimental)
   if (!globalThis.crypto) {
@@ -7,20 +10,20 @@ try {
       configurable: true,
       enumerable: false,
     });
-    console.log('Having to shim crypto...");
+    console.log('Having to shim crypto...');
   }
 } catch (e) {
   console.warn('No Crypto available:', e);
 }
 
-
-import { ApimApiPlugin } from './plugin'
-import { ApimApiUtils } from './functions'
+// Use require so TS doesn't hoist imports above the shim
+const { ApimApiPlugin } = require('./plugin');
+const { ApimApiUtils } = require('./functions');
 
 /*  comment out these entries if you are not including an ingredient plugin runner*/
-exports.plugin = ApimApiPlugin
-exports.pluginNS = "@azbake/ingredient-apim-api" //name of the ingredient to reference in a bake.yaml recipe
+exports.plugin = ApimApiPlugin;
+exports.pluginNS = '@azbake/ingredient-apim-api'; // name of the ingredient to reference in a bake.yaml recipe
 
 /* comment out these entries if you are not including a set of expression functions*/
-exports.functions = ApimApiUtils
-exports.functionsNS = "apimapi" //bake.yaml expressions can access your functions via "myutils.my_function()"
+exports.functions = ApimApiUtils;
+exports.functionsNS = 'apimapi'; // bake.yaml expressions can access your functions via "myutils.my_function()"
